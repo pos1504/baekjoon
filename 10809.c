2510b0978e437a12815dc3ac25839575dac9f832
pos1504 <pos1504@gmail.com>
2022-04-29 01:27:09 +0900

    20220429

--- 10809.c
+++ 10809.c
@@ -1,19 +1,27 @@
 #include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-
-char FindAlphabet()
+int FindAlphabet()
 {
-	char string[110];
+	char string[100] = { 0, };//초기화 해주자
+	int AZstring[26] = { 0, };//초기화 해주자
+	scanf("%s", string);
 
-	scanf_s("%s", string,110);
 
-	for (int i = 0; i <=100  ; i++)
+	for (int i = 0; i < 26; i++) 
+	{
+		AZstring[i] = -1;    //AZstring -1로 전부 초기화
+	}
+	
+
+	for (int i = 0; i <100  ; i++) /
 	{
 		for (int j = 97; j <= 122; j++)
 		{
-			if ((int)string[i] == j)
+			if ((int)string[i] == j) 
 			{
-				printf("%d ", i);
+				if (AZstring[j - 97] != -1) break; //첫번째 등장한 알파벳의 배열 위치를 선정하기위해 break
+				AZstring[j-97] = i;
 			}
 			
 		}
@@ -21,6 +29,12 @@ char FindAlphabet()
 
 	}
 
+	for (int i = 0; i < 26; i++)
+	{
+		printf("%d ", AZstring[i]);
+	}
 
+
+	return 0;
 
 }
